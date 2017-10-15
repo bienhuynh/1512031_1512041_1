afd5199e4d3a26ab96ca114089e48ee26ee06ded
unknown <bienhuynh96@gmail.com>
2017-10-15 12:21:54 +0700

    cau truc do thi

--- Yeu_Cau_1/Source_1_1/Source_1_1/Main.cpp
+++ Yeu_Cau_1/Source_1_1/Source_1_1/Main.cpp
@@ -1,8 +1,33 @@
 #include "iostream";
 using namespace std;
 
+//create and manage node
+struct Node
+{
+	int data;
+	Node **pNext;
+};
+
+struct DoubleNode
+{
+	//node start S
+	Node S;
+	//node end G
+	Node G;
+};
+
+//List node graph
+Node *nodeList = new Node();
+
+//read file input input.txt
+
+
+//search algothrium
+
+
 void main()
 {
-	printf("Xin chao");
+	//read input file
+
 	system("pause");
 }
